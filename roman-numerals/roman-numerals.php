483bf8f88185e08c3b0d8c19bcefa9f0abee1a9c
marcware <Salvat@1982>
2019-05-20 22:45:45 +0200

    ROMAN: green pasamos el test para el 5

--- roman-numerals/roman-numerals.php
+++ roman-numerals/roman-numerals.php
@@ -5,7 +5,8 @@ function toRoman($arabic)
     $roman = '';
     if ($arabic === 4) {
         $roman = 'IV';
-
+    } else if ($arabic === 5) {
+        $roman = 'V';
     } else {
         for ($i = 0; $i < $arabic; $i++) {
             $roman = $roman . 'I';
