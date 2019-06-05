8e2b5487cb77e33532efa3e4d73d4f5aa840ac72
marcware <Salvat@1982>
2019-06-05 22:55:39 +0200

    GIGASECOND: REFACTORING ponemos lo que devuelve la clase

--- gigasecond/gigasecond.php
+++ gigasecond/gigasecond.php
@@ -2,7 +2,12 @@
 
 const GIGA_SECOND = 'PT1000000000S';
 
-function from(DateTime $date)
+/**
+ * @param DateTime $date
+ * @return DateTime
+ * @throws Exception
+ */
+function from(DateTime $date) :DateTime
 {
     $dateClone = clone $date;
     return $dateClone->add(new DateInterval(GIGA_SECOND));
