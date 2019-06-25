e644e51911347ca1421ea5a652d6228e553d4613
marcware <Salvat@1982>
2019-06-25 19:48:03 +0200

    BOB: GREEN test Shouting

--- bob/bob.php
+++ bob/bob.php
@@ -5,7 +5,9 @@ class Bob
 {
     public function respondTo($sentence): string
     {
-
+        if (mb_strstr($sentence,'!')) {
+            return 'Whoa, chill out!';
+        }
         return "Whatever.";
     }
 }
